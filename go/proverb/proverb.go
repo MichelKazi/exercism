2ee07ad74271b9866eb5f129adda1168b38d16b1
Michelkazi <michel.m.kazi@gmail.com>
2020-02-02 20:50:56 -0500

    documented proverb.go

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -1,14 +1,14 @@
 // This is a "stub" file.  It's a little start on your solution.
 // It's not a complete solution though; you have to write some code.
 
-// Package proverb should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package proverb contains a function that returns a proverbial rhyme
 package proverb
 
 import "fmt"
 
 // Proverb should have a comment documenting it.
 // Proverb takes an array of strings and returns a proverbial rhyme
+// If it's empty, nothing happens
 func Proverb(rhyme []string) []string {
 
 	if len(rhyme) == 0 {
