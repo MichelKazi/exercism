accd086f2f00676df3e650c867da574aecdc55d5
Michelkazi <michel.m.kazi@gmail.com>
2020-02-02 20:48:48 -0500

    passed tests for proverb

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -8,13 +8,21 @@ package proverb
 import "fmt"
 
 // Proverb should have a comment documenting it.
-// Proverb takes an array of strings and returns
+// Proverb takes an array of strings and returns a proverbial rhyme
 func Proverb(rhyme []string) []string {
-	var arr = []string{}
 
-	for i := 0; i < len(rhyme)-1; i++ {
-		arr = append(arr, fmt.Sprintf("For want of a %s the %s was lost.\n", rhyme[i], rhyme[i+1]))
+	if len(rhyme) == 0 {
+		return []string{}
 	}
 
-	return append(arr, fmt.Sprintf("And all for the want of a %s\n", rhyme[0]))
+	arr := []string{}
+	allForWant := fmt.Sprintf("And all for the want of a %s.", rhyme[0])
+
+	for i := 0; i < len(rhyme)-1; i++ {
+		arr = append(arr, fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i], rhyme[i+1]))
+	}
+
+	arr = append(arr, allForWant)
+
+	return arr
 }
