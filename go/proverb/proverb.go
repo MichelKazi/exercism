a2169c8d03b574cb42391e662b624e85ad2f167e
Michelkazi <michel.m.kazi@gmail.com>
2020-02-02 20:27:36 -0500

    failing tests for proverb, time to debug

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -5,11 +5,16 @@
 // https://golang.org/doc/effective_go.html#commentary
 package proverb
 
+import "fmt"
+
 // Proverb should have a comment documenting it.
+// Proverb takes an array of strings and returns
 func Proverb(rhyme []string) []string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return []string{}
+	var arr = []string{}
+
+	for i := 0; i < len(rhyme)-1; i++ {
+		arr = append(arr, fmt.Sprintf("For want of a %s the %s was lost.\n", rhyme[i], rhyme[i+1]))
+	}
+
+	return append(arr, fmt.Sprintf("And all for the want of a %s\n", rhyme[0]))
 }
